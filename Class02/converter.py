3db0fdf70b2331c2973018f7d09274a687fa8abf
Cesar <cesar.zamora.lander@gmail.com>
2021-01-27 21:20:02 -0500

    Class 02 completed

--- Class02/converter.py
+++ Class02/converter.py
@@ -8,7 +8,7 @@ f = ((9/5) * celcius) + 32
 print("The temp in f is {:2.2f}", format(f))
 
 # 𝐶 = (𝐹 − 32)/ (9/5)
-f =17.6
+f = 17.6
 c = (f - 32) / (9 / 5)
 
 print("The temp in c is {:2.2f}", format(c))
